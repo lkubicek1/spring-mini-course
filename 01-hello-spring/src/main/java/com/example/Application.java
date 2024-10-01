b3017598070f2ac3d95ff981c2684111f4fa0527
lkubicek1 <lkubicek1@gmail.com>
2024-10-01 09:29:28 -0400

    Refactored to support infrastructure

--- 01-hello-spring/src/main/java/com/example/Application.java
+++ 01-hello-spring/src/main/java/com/example/Application.java
@@ -1,20 +1,22 @@
 package com.example;
 
-import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Slf4j
 @SpringBootApplication
 public class Application implements CommandLineRunner {
+
+    private static final Logger log = LogManager.getLogger(Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         log.info("Hello Spring!");
     }
 }
